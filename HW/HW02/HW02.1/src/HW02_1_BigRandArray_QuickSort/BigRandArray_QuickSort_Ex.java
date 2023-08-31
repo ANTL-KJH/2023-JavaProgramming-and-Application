a2007ae21b1399120a2a57e0cc41fe461d8ff357
ANTL-KJH <wogus9190@gmail.com>
2023-08-31 19:16:48 +0900

    Update HW02.1

--- HW/HW02/HW02.1/src/HW02_1_BigRandArray_QuickSort/BigRandArray_QuickSort_Ex.java
+++ HW/HW02/HW02.1/src/HW02_1_BigRandArray_QuickSort/BigRandArray_QuickSort_Ex.java
@@ -1,9 +1,21 @@
+/*
+ * Project Name : JavaProgramming and Application HW02.1
+ * Program's purpose and basic function :
+ * - BigRandArray
+ * Program First Author : JH KIM
+ * Date of First Write : 2023.08.31
+ * =======================================================================================================
+ * Source Code Modification History
+ * =======================================================================================================
+ * Author				Date				Ver				Modification details
+ * JH KIM				2023.08.31			v1.00			First Write
+ */
 package HW02_1_BigRandArray_QuickSort;
 
 import java.util.Scanner;
 
 public class BigRandArray_QuickSort_Ex {
-    public static int[] genBigRandIntArray(int size, int offset)
+    public static int[] genBigRandIntArray(int size, int offset)    // genBigRandIntArray, shuffle 방식
     {
         int[] bigIntArray = new int[size];
         int j, temp;
@@ -11,7 +23,7 @@ public class BigRandArray_QuickSort_Ex {
             bigIntArray[i] = i + offset;
         /* shuffle */
         for(int i=0; i<size; i++) {
-            j = (int) (Math.random() * size);   // Math.random()은 0.0~1.0 사이의 소수를 랜덤하게 생성
+            j = (int) (Math.random() * size);   // Math.random()은 0.0~1.0 사이의 소수를 랜덤하게 생성, 생성 후 int형으로 변환
             if (j == i)
                 continue;
             temp = bigIntArray[i];
@@ -20,13 +32,15 @@ public class BigRandArray_QuickSort_Ex {
         }
         return bigIntArray;
     }
-    public static void printBigArraySample(int[] bigArray,int size,int per_line,int sample_lines)
+    public static void printBigArraySample(int[] bigArray,int size,int per_line,int sample_lines)   // printBigArraySample
     {
-        int last_block_start;
         int count = 0;
-        for (int i=0; i<sample_lines; i++) {
-            for (int j = 0; j < per_line; j++) {
-                if (count > size) {
+        for (int i=0; i<sample_lines; i++)
+        {
+            for (int j = 0; j < per_line; j++)
+            {
+                if (count > size)   // if count exceeds the size, then return
+                {
                     System.out.print("\n");
                     return;
                 }
@@ -38,9 +52,12 @@ public class BigRandArray_QuickSort_Ex {
         if (count < (size - per_line * sample_lines))
             count = size - per_line * sample_lines;
         System.out.print("\n . . . . . \n");
-        for (int i = 0; i < sample_lines; i++) {
-            for (int j = 0; j< per_line; j++) {
-                if (count > size) {
+        for (int i = 0; i < sample_lines; i++)
+        {
+            for (int j = 0; j< per_line; j++)
+            {
+                if (count > size)   // if count exceeds the size, then return
+                {
                     System.out.print("\n");
                     return;
                 }
@@ -53,23 +70,29 @@ public class BigRandArray_QuickSort_Ex {
     }
     public static int _partition(int[] array, int size, int left,int right,int pivotIndex)
     {
-        int pivotValue; // pivot value
-        int newPI; // new pivot index
+        int pivotValue;
+        int newPI;
         int temp, i;
+
         pivotValue = array[pivotIndex];
+
+        // swap pivot index with end index
         temp = array[pivotIndex];
         array[pivotIndex] = array[right];
-        array[right] = temp; // Move pivot to end
+        array[right] = temp;
+
         newPI = left;
-        for (i = left; i <= (right - 1); i++) {
-            if (array[i] <= pivotValue) {
+        for (i = left; i <= (right - 1); i++)
+        {
+            if (array[i] <= pivotValue) // swap
+            {
                 temp = array[i];
                 array[i] = array[newPI];
                 array[newPI] = temp;
                 newPI = newPI + 1;
             }
         }
-// swap array[storeIndex] and array[right]; Move pivot to its final place
+        // swap pivot index with end
         temp = array[newPI];
         array[newPI] = array[right];
         array[right] = temp;
@@ -78,19 +101,19 @@ public class BigRandArray_QuickSort_Ex {
     public static void _quickSort(int[] array, int size, int left, int right)
     {
         int pI, newPI; // pivot index
-        if (left >= right) {
+        if (left >= right)
+        {
             return;
         }
-        pI = (left + right) / 2;
+        pI = (left + right) / 2;    // pivot Index
         newPI = _partition(array, size, left, right, pI);
-        if (left < (newPI - 1)) {
+        if (left < (newPI - 1))     // partition left side
+        {
             _quickSort(array, size, left, newPI - 1);
-// recursively sort elements on the left of pivotNewIndex
-        } if ((newPI +
-            1) <
-            right) {
+        }
+        if ((newPI + 1) < right)    // partition right side
+        {
         _quickSort(array, size, newPI + 1, right);
-// recursively sort elements on the right of pivotNewIndex
         }
     }
     public static void quickSort(int[] array, int size)
@@ -107,7 +130,7 @@ public class BigRandArray_QuickSort_Ex {
             big_size = cin.nextInt();
             if (big_size == 0)
                 break;
-            bigRandIntArray = genBigRandIntArray(big_size, offset);
+            bigRandIntArray = genBigRandIntArray(big_size, offset); // genBigRandArray
             System.out.printf("Before sorting, size = %d, offset = %d\n", big_size, offset);
             printBigArraySample(bigRandIntArray, big_size, 10, 2);
             quickSort(bigRandIntArray, big_size);
