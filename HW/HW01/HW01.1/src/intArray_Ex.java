2150a5eb45fff01f1828cbba01cb0a8e8c10ec92
ANTL-KJH <wogus9190@gmail.com>
2023-09-05 14:28:34 +0900

    Update HW01

--- HW/HW01/HW01.1/src/intArray_Ex.java
+++ HW/HW01/HW01.1/src/intArray_Ex.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Project Name : JavaProgramming and Application HW01.1
  * Program's purpose and basic function :
  * - Sort and print the input int array
@@ -10,39 +10,40 @@
  * Author				Date				Ver				Modification details
  * JH KIM				2023.08.29			v1.00			First Write
  */
+
 import java.util.Scanner;
 
 public class intArray_Ex {
 
-    public static int[] get_int_array(Scanner cin, int size) {
-        int[] arr = new int[size];
+    public static int[] get_int_array(Scanner cin, int size)    // make array and get integer data
+    {
+        int[] arr = new int[size];  // make array
         int num = 0;
         for (int i = 0; i < size; i++) {
-            num = cin.nextInt();
+            num = cin.nextInt();    // get integer data
             arr[i] = num;
         }
         return arr;
     }
 
-    public static void print_array(int[] array, int size) {
-
-        for (int i=0;i<size;i++)
-        {
-             System.out.print(array[i]+" ");
+    public static void print_array(int[] array, int size)   // print array
+    {
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + " ");
         }
         System.out.print("\n");
     }
 
-    public static void insertion_sort(int[] array, int size) {
-        int temp, i, j;
-        for (i = 1; i <= (size-1); i++)
-        {
+    public static void insertion_sort(int[] array, int size)    // insertion sort
+    {
+        int temp, i, p;
+        for (i = 1; i <= (size - 1); i++) {
             temp = array[i];
-            for (j = i; j > 0 && array[j - 1] >= temp; j--)
+            for (p = i; p > 0 && array[p - 1] >= temp; p--) // make space
             {
-                array[j] = array[j-1]; // shift right to make a room
+                array[p] = array[p - 1];
             }
-            array[j] = temp;
+            array[p] = temp;    // insertion
         }
     }
 
--- HW/HW01/HW01.2/src/Dyn_Fibonacci.java
+++ HW/HW01/HW01.2/src/Dyn_Fibonacci.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Project Name : JavaProgramming and Application HW01.2
  * Program's purpose and basic function :
  * - Dynamic Fibonacci Calculator
@@ -9,14 +9,14 @@
  * =======================================================================================================
  * Author				Date				Ver				Modification details
  * JH KIM				2023.08.30			v1.00			First Write
+ * JH KIM               2023.09.05          v1.01           add comment
  */
-import java.util.Arrays;
 import java.util.Scanner;
 public class Dyn_Fibonacci {
     final static int max_n = 1000;
     static double[] fb_n_tbl =new double[max_n];
     static boolean fb_n_tbl_initialized = false;
-    public static double Dyn_Fibonacci(int n)
+    public static double Dyn_Fibonacci(int n)   // Dynamic fibonacci
     {
         if (!fb_n_tbl_initialized) {
             for (int i=0; i<max_n; i++)
@@ -35,11 +35,11 @@ public class Dyn_Fibonacci {
             fb_n_tbl[n] = n;
             return n;
         }
-        else
+        else    // fibo_n didn't calculate case
         {
-            double fb_n = Dyn_Fibonacci(n-2) + Dyn_Fibonacci(n-1);
-            fb_n_tbl[n] = fb_n;
-            return fb_n;
+            double fibo_n = Dyn_Fibonacci(n-2) + Dyn_Fibonacci(n-1);
+            fb_n_tbl[n] = fibo_n;
+            return fibo_n;
         }
     }
     public static void main(String[] args)
@@ -50,7 +50,7 @@ public class Dyn_Fibonacci {
         while (true)
         {
             System.out.print("input n (>=0) to find 0 ~ nth fibo_n(-1 to terminate) : ");
-            n = cin.nextInt();
+            n = cin.nextInt();  // input fibo_n
             if(n == -1)
                 break;
             for (int i=0; i<=n;i++)
