eb299aad239494bf8b5965ea97b2bef587ace429
ANTL-KJH <wogus9190@gmail.com>
2023-10-27 16:04:15 +0900

    Update ch07

--- ch07/src/ch07_3_2_KnapsackProblem_01/App_knapsackProblem01.java
+++ ch07/src/ch07_3_2_KnapsackProblem_01/App_knapsackProblem01.java
@@ -12,22 +12,14 @@ public class App_knapsackProblem01 {
         System.out.print(" Processing to obtain best solution by bruteforce() :\n ");
         sol = kp.BruteForce_KP01();
         System.out.print("Solution obtained by bruteforce() :\n ");
-        printSolution(sol);
+        kp.printSolution(sol);
         System.out.print(" Processing to obtain best solution by DynamicProgramming() :\n ");
-        sol = kp.DynProg_KP01();
+        sol = kp.DynKSP_BottomUp();
         System.out.print("Solution obtained by DynamicProgramming() :\n ");
-        printSolution(sol);
-    }
-    public static void printSolution(KSP_Solution sol) {
-        System.out.printf("total value = %d, total weight = %d, selected items = [", sol.totalValue, sol.totalWeight);
-        int num_items = sol.selectedItems.size();
-        for (int i=0; i<num_items; i++) {
-            System.out.printf("%s", sol.selectedItems.get(i));
-            if (i != (num_items -1)) {
-                System.out.printf(", ");
-            } else {
-                System.out.printf("]\n");
-            }
-        }
+        kp.printSolution(sol);
+        System.out.printf(" Processing to obtain best solution by DynKSP_TopDown() :\n ");
+        sol = kp.DynKSP_TopDown();
+        System.out.printf("Solution obtained by DynKSP_TopDown() :\n ");
+        kp.printSolution(sol);
     }
 }
--- ch07/src/ch07_3_2_KnapsackProblem_01/Knapsack01.java
+++ ch07/src/ch07_3_2_KnapsackProblem_01/Knapsack01.java
@@ -54,7 +54,7 @@ public class Knapsack01 {
         solution.totalWeight = bestSubsetWeight;
         return solution;
     }
-    public KSP_Solution DynProg_KP01() {
+    public KSP_Solution DynKSP_BottomUp() {
         KSP_Solution[][] m = new KSP_Solution[this.items.length + 1][this.capacity + 1];
         for (int i=0; i<= this.items.length; i++) {
             for (int w=0; w<= this.capacity; w++) {
@@ -87,5 +87,65 @@ public class Knapsack01 {
         KSP_Solution solution = new KSP_Solution();
         solution = m[this.items.length][this.capacity];
         return solution;
+    }
+    public KSP_Solution _dynKSP_recursive(int index, int avail, KSP_Solution[][] solTbl) {
+        KSP_Solution solution = new KSP_Solution();
+        if ((index >= this.items.length) || (avail <= 0)) {
+            solution = new KSP_Solution();
+            solution.selectedItems = new ArrayList<Item>();
+            solution.totalValue = 0;
+            solution.totalWeight = 0;
+            return solution;
+        } else if (solTbl[index][avail] != null) {
+            return solTbl[index][avail];
+        } else if (this.items[index].weight > avail) {
+            solution = _dynKSP_recursive(index + 1, avail, solTbl);
+        } else {
+            Item candidateItem = this.items[index];
+            KSP_Solution solution_with = new KSP_Solution();
+            KSP_Solution solution_without = new KSP_Solution();
+            solution_with = _dynKSP_recursive(index + 1, avail - candidateItem.weight, solTbl);
+            solution_with.totalValue += candidateItem.value;
+            solution_with.totalWeight += candidateItem.weight;
+            solution_with.selectedItems.add(candidateItem);
+            solution_without = _dynKSP_recursive(index + 1, avail, solTbl);
+            if (solution_with.totalValue > solution_without.totalValue) {
+                solution = solution_with;
+            } else {
+                solution = solution_without;
+            }
+        }
+        solTbl[index][avail] = new KSP_Solution();
+        solTbl[index][avail].selectedItems = solution.selectedItems;
+        solTbl[index][avail].totalValue = solution.totalValue;
+        solTbl[index][avail].totalWeight = solution.totalWeight;
+        return solution;
+    }
+    public KSP_Solution DynKSP_TopDown() {
+        KSP_Solution[][] solutionTable = new KSP_Solution[this.items.length][this.capacity + 1];
+        for (int k=0; k< this.items.length; k++) {
+            for (int w=1; w<= this.capacity; w++) {
+                solutionTable[k][w] = null;
+            }
+            solutionTable[k][0] = new KSP_Solution();
+            solutionTable[k][0].selectedItems = new ArrayList<Item>();
+            solutionTable[k][0].totalValue = 0;
+            solutionTable[k][0].totalWeight = 0;
+        }
+        int index_start = 0;
+        KSP_Solution solution = _dynKSP_recursive(index_start, this.capacity, solutionTable);
+        return solution;
+    }
+    public static void printSolution(KSP_Solution sol) {
+        System.out.printf("total value = %d, total weight = %d, selected items = [", sol.totalValue, sol.totalWeight);
+        int num_items = sol.selectedItems.size();
+        for (int i=0; i<num_items; i++) {
+            System.out.printf("%s", sol.selectedItems.get(i));
+            if (i != (num_items -1)) {
+                System.out.printf(", ");
+            } else {
+                System.out.printf("]\n");
+            }
+        }
     }
 }
